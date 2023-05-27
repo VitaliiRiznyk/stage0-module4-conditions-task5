43dd612d8e03bb8608c14cf2073756f363826c80
VitaliiRiznyk <vitaliiriznyk@gmail.com>
2023-05-27 20:04:10 +0300

    h

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result;
+
+        switch (year % 4) {
+            case 0:
+                switch (year % 100) {
+                    case 0:
+                        switch (year % 400) {
+                            case 0:
+                                result = "leap";
+                                break;
+                            default:
+                                result = "not leap";
+                                break;
+                        }
+                        break;
+                    default:
+                        result = "leap";
+                        break;
+                }
+                break;
+            default:
+                result = "not leap";
+                break;
+        }
+        System.out.println(result);
     }
 }
